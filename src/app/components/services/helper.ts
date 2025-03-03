006d9714db3c07c44923db81e68482223956a64c
unknown <emmanueldery31@gmail.com>
2025-03-03 20:56:49 +0000

    Commiting images added

--- src/app/components/services/helper.ts
+++ src/app/components/services/helper.ts
@@ -1,7 +1,7 @@
  //LOCAL API ACCESS
-//  let baseUrl = 'http://localhost:9191/api/v1/auth';
+ let baseUrl = 'http://localhost:9191/api/v1/auth';
 
 
  //ONLINE HOSTING
-let baseUrl = 'https://databundlbackend.onrender.com/api/v1/auth';
+// let baseUrl = 'https://databundlbackend.onrender.com/api/v1/auth';
 export default baseUrl;
