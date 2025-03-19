340cd830f38db69c7b2cd2eebbfbce1e2b376259
unknown <emmanueldery31@gmail.com>
2025-03-19 14:00:27 +0000

    Commiting Modal

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,TemplateRef,ViewChild} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+
+    @ViewChild('paymentModal') paymentModal!: TemplateRef<any>; // Reference to the modal template
+
+    constructor(private dialog: MatDialog,
+    ){ }
+  
+  openModal(recipient: string) {
+       // Open the modal
+    const dialogRef = this.dialog.open(this.paymentModal, {
+      width: '700px',
+      disableClose: true // Prevent closing by clicking outside
+    });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('The modal was closed');
+    //   // Handle the result if needed
+    // });
+  }
 
 }
